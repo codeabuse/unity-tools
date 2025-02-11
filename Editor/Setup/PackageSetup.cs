4889099ae5d4b6c1a502ea583955e792c4b8d57d
Alexander Komarov <komarov@medvr.pro>
2025-02-11 13:31:40 +0300

    checking for UniTask package and warning user if it is not found or is installed as a local plugin

---
+++ Editor/Setup/PackageSetup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+
+namespace Codeabuse
+{
+    public static class PackageSetup
+    {
+        [InitializeOnLoadMethod]
+        private static void CheckDependencies()
+        {
+            #if (!UNITASK_ENABLED)
+            {
+                EditorUtility.DisplayDialog("Unity Tools package setup",
+                        "UniTask integration is disabled. \n To enable it, install UniTask package via Package Manager" +
+                        " using Git URL (see https://github.com/Cysharp/UniTask?tab=readme-ov-file#upm-package for instructions).",
+                        "Ok");
+            }
+            #endif
+        }
+    }
+}
