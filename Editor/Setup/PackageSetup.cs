a63b74f90001f5669450b145024070709b67f2a7
Alexander Komarov <komarov@medvr.pro>
2025-02-18 15:54:54 +0300

    PackageSetup: check fresh install/upgrade before user prompt

--- Editor/Setup/PackageSetup.cs
+++ Editor/Setup/PackageSetup.cs
@@ -1,20 +1,76 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
+using UnityEngine;
 
 namespace Codeabuse
 {
-    public static class PackageSetup
+    internal static class PackageSetup
     {
+        private const string package_id = "net.codeabuse.unity-tools";
+        private const string setup_done_key = "setup_done";
+
+        private static ListRequest _packagesRequest;
+        
         [InitializeOnLoadMethod]
         private static void CheckDependencies()
         {
-            #if (!UNITASK_ENABLED)
+            _packagesRequest = Client.List();
+            EditorApplication.update += OnEditorUpdate;
+        }
+
+        private static void OnEditorUpdate()
+        {
+            switch (_packagesRequest.Status)
+            {
+                case StatusCode.InProgress:
+                    return;
+                case StatusCode.Success:
+                    break;
+                case StatusCode.Failure:
+                    EditorApplication.update -= OnEditorUpdate;
+                    Debug.LogError("Unity Tools: Can't determine Package Version - Package Manager List error");
+                    return;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            
+            EditorApplication.update -= OnEditorUpdate;
+            
+            string packageVersion = "0";
+            foreach (var package in _packagesRequest.Result)
+            {
+                if (package.name != package_id)
+                    continue;
+                packageVersion = package.version;
+                break;
+            }
+
+            _packagesRequest = null;
+
+            if (Version.TryParse(ProjectPrefs.GetString(setup_done_key, "0"), out var lastVersion) &&
+                Version.TryParse(packageVersion, out var currentVersion))
+            {
+                if (currentVersion.CompareTo(lastVersion) > 0)
+                {
+                    ProjectPrefs.SetString(setup_done_key, packageVersion);
+                    PromptUserWithPackageInfo();
+                }
+            }
+        }
+
+        private static void PromptUserWithPackageInfo()
+        {
+            // TODO: integrate more dependency cheks and show dedicated PackageInfoWindow with nicely formatted URLs.
+#if (!UNITASK_ENABLED)
             {
                 EditorUtility.DisplayDialog("Unity Tools package setup",
                         "UniTask integration is disabled. \n To enable it, install UniTask package via Package Manager" +
                         " using Git URL (see https://github.com/Cysharp/UniTask?tab=readme-ov-file#upm-package for instructions).",
                         "Ok");
             }
-            #endif
+#endif
         }
     }
 }
---
+++ Runtime/Common/Version.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+
+namespace Codeabuse
+{
+    public readonly struct Version : IComparable<Version>
+    {
+        private readonly uint[] _registers;
+
+        public Version(params uint[] registers)
+        {
+            if (registers.Length == 0)
+                throw new ArgumentException("Version must have at least one digit");
+            _registers = registers;
+        }
+
+        public uint[] Registers => _registers;
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendJoin('.', _registers);
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Alphabetical symbols are not supported. Only numerical representations, like "1.0.0".
+        /// </summary>
+        public static bool TryParse(string versionString, out Version version)
+        {
+            version = default;
+            if (string.IsNullOrEmpty(versionString))
+            {
+                return false;
+            }
+            var split = versionString.Split('.');
+            var registers = new uint[split.Length];
+            for (var i = 0; i < split.Length; i++)
+            {
+                if (uint.TryParse(split[i], out registers[i]))
+                    continue;
+                return false; //TODO: add support for alphabetical symbols (i.e. "1.0.1a")
+            }
+
+            version = new Version(registers);
+            return true;
+        }
+
+        public int CompareTo(Version other)
+        {
+            var minRegister = Math.Min(Registers.Length, other.Registers.Length);
+            for (var i = 0; i < minRegister; i++)
+            {
+                var diff = (int)Registers[i] - (int)other.Registers[i];
+                if (diff != 0)
+                    return diff;
+            }
+
+            return 0;
+        }
+    }
+}
