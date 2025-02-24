7e4f562074b9d4922cbb12b8feef49f98339cdd3
Alexander Komarov <komarov@medvr.pro>
2025-02-24 12:32:43 +0300

    QuitButtonHandler component

---
+++ Runtime/UI/QuitButtonHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Codeabuse.UI
+{
+    public class QuitButtonHandler : MonoBehaviour
+    {
+        [SerializeField]
+        private UnityEvent _onQuitRequest = new();
+        
+        public void Quit()
+        {
+            _onQuitRequest.Invoke();
+            Application.Quit();
+        }       
+    }
+}
