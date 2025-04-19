78565d11f6459de2b435531c09d4ad4dfc26df58
Alexander Komarov <komarov@medvr.pro>
2025-04-19 16:24:57 +0300

    mask property attribute and drawer

---
+++ Runtime/PropertyAttributes/MaskAttribute.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace VRSim.ApplicationCore.PropertyAttributes
+{
+    public class MaskAttribute : PropertyAttribute
+    {
+        public int LayersCount { get; }
+
+        public MaskAttribute(int layersCount = 16)
+        {
+            LayersCount = Mathf.Clamp(layersCount, 2, 64);
+        }
+    }
+}
