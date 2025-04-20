68049035cd18a87997dddae22e53f6d1beb50d6d
Alexander Komarov <komarov@medvr.pro>
2025-04-20 15:43:36 +0300

    mask property attribute and drawer

---
+++ Editor/PropertyDrawers/MaskPropertyDrawer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.Pool;
+using UnityEngine.UIElements;
+
+namespace Codeabuse
+{
+    [CustomPropertyDrawer(typeof(MaskAttribute))]
+    public class MaskPropertyDrawer : PropertyDrawer
+    {
+        private static readonly Dictionary<int, List<string>> generated_names_lists = new();
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            var layersCount = ((MaskAttribute)attribute).LayersCount;
+            var choices = generated_names_lists.GetValueOrDefault(layersCount, 
+                GetChoicesNames(layersCount));
+            var maskField = new MaskField(property.displayName, choices, 0)
+            {
+                bindingPath = property.propertyPath
+            };
+            return maskField;
+        }
+
+        private static List<string> GetChoicesNames(int layersCount)
+        {
+            var list = ListPool<string>.Get();
+            for (var i = 0; i < layersCount; i++)
+            {
+                list.Add(i.ToString());
+            }
+            return list;
+        }
+    }
+}
---
+++ Runtime/PropertyAttributes/MaskAttribute.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Codeabuse
+{
+    public class MaskAttribute : PropertyAttribute
+    {
+        public int LayersCount { get; }
+
+        public MaskAttribute(int layersCount = 16)
+        {
+            LayersCount = Mathf.Clamp(layersCount, 2, 64);
+        }
+    }
+}
